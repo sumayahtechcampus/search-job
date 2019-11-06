6d208e58a01c8d37be6280c56e3676725f95a7f9
sumayahtechcampus <sumayah.ali.saleh@gmail.com>
2019-11-06 16:18:04 +0300

    Updates

---
+++ precache-manifest.98cf2c3c4617eafe47205223497f0d43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5024e89fed4dac83a3df0561c9b3cdc5",
+    "url": "/search-job/index.html"
+  },
+  {
+    "revision": "b6b845ab89afec1ab6fd",
+    "url": "/search-job/static/css/main.874e8e16.chunk.css"
+  },
+  {
+    "revision": "571471e28db1fbcc8c08",
+    "url": "/search-job/static/js/2.6a276d95.chunk.js"
+  },
+  {
+    "revision": "b6b845ab89afec1ab6fd",
+    "url": "/search-job/static/js/main.aafe8c6a.chunk.js"
+  },
+  {
+    "revision": "81841ccc2e9f862bb136",
+    "url": "/search-job/static/js/runtime-main.c85a78a7.js"
+  },
+  {
+    "revision": "b30c7570fc7a66015c22ce42d353c19f",
+    "url": "/search-job/static/media/data.b30c7570.jpg"
+  },
+  {
+    "revision": "e1461412b0cf6268a1288f0b1f1bfa6c",
+    "url": "/search-job/static/media/j1.e1461412.png"
+  },
+  {
+    "revision": "532e920c7e1704c752fdea8487c98428",
+    "url": "/search-job/static/media/joo.532e920c.jpg"
+  },
+  {
+    "revision": "3bdc17c6ab6e473e69e5e2a36521ff77",
+    "url": "/search-job/static/media/logo.3bdc17c6.jpg"
+  },
+  {
+    "revision": "3a02b905426cb8aadbbe0a42aea6042b",
+    "url": "/search-job/static/media/web.3a02b905.jpg"
+  }
+]);
